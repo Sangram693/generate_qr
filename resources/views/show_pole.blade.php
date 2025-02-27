45fb219735b99cc3bbfdc7c4fb6a56f1b9b83cce
sneid <sneider.ele@gmail.com>
2025-02-27 17:52:23 +0530

    add verify to pole

--- resources/views/show_pole.blade.php
+++ resources/views/show_pole.blade.php
@@ -7,12 +7,21 @@
 
     <!-- Bootstrap for Styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <!-- jQuery for AJAX -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </head>
 <body>
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="message-box _success">
+                <i class="fa fa-check-circle"></i>
+                <h2>Utkarsh Product Verified</h2>
+            </div>
+        </div>
+    </div>
+
     <div class="container mt-5">
         <h2 class="text-center">Pole Details</h2>
         
