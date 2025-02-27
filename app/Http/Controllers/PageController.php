2b467b01cfd0e1ad2cf3a4968c40715e2e8c08f4
sneid <sneider.ele@gmail.com>
2025-02-27 18:02:40 +0530

    change token

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -51,9 +51,9 @@ class PageController extends Controller
         };
 
         $baseUrl = match ($request->product_type) {
-            'w-beam' => 'http://utkarshsmart.in/api/w-beam/',
-            'pole' => 'http://utkarshsmart.in/api/pole/',
-            'high-mast' => 'http://utkarshsmart.in/api/high-mast/',
+            'w-beam' => 'http://verify.utkarshsmart.in/api/w-beam/',
+            'pole' => 'http://verify.utkarshsmart.in/api/pole/',
+            'high-mast' => 'http://verify.utkarshsmart.in/api/high-mast/',
         };
 
         [$r, $g, $b] = match ($request->product_type) {
