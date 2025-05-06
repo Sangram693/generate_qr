d783670413c9e7089a508d0e73738ed6655dd98c
Sangram693 <sneider@cbsiot.live>
2025-05-06 13:15:04 +0530

    make header in footer

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -168,12 +168,11 @@ class PageController extends Controller
 
 
 
-        // $pdf->SetFillColor(255, 255, 255, 0); 
-        // $pdf->Rect(0, 0, $page_width, $page_height, 'F');
-        $pdf->SetFont('helvetica', 'B', 12);
-        $pdf->SetTextColor($r, $g, $b);
-        $pdf->SetXY(0, 0); // Adjust position if needed
-        $pdf->Cell(0, 0, $headerText, 0, 1, 'C', false);
+        $bottomY = $page_height - $margin_bottom + 2; // Adjust +2 to push slightly below the content
+$pdf->SetFont('helvetica', 'B', 12);
+$pdf->SetTextColor($r, $g, $b);
+$pdf->SetXY(0, $bottomY);
+$pdf->Cell(0, 10, $headerText, 0, 1, 'C', false);
         
         foreach ($data as $productId) {
             $product = match ($request->product_type) {
